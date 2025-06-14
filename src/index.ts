17aaa52336b6db01169b4708c02c6a098cf0f011
Madson de Carvalho <madson.de.carvalho@gmail.com>
2025-06-14 13:19:35 -0300

    feat: adicionando Redis

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-// src/index.ts
 import express, { ErrorRequestHandler } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 import clientRoutes from './routes/clientRoutes';
+import { redisService } from './services/RedisService';
+
+import 'dotenv/config';
+
 
 dotenv.config();
 const app = express();
@@ -15,6 +18,21 @@ app.use(express.json());
 
 // conectar ao Mongo
 connectDB();
+
+redisService
+  .set('healthcheck', 'ok', 10)
+  .then(() => redisService.get('healthcheck'))
+  .then((value) => {
+    if (value === 'ok') {
+      console.log('✅ Conexão com Redis estabelecida!');
+    } else {
+      console.error('⚠️ Falha ao validar conexão com Redis.');
+    }
+  })
+  .catch((err) => {
+    console.error('❌ Erro ao conectar ao Redis:', err);
+  });
+
 
 // rotas
 app.use('/api', clientRoutes);
---
+++ src/services/RedisService.ts
@@ -0,0 +1,31 @@
+import Redis from 'ioredis';
+
+class RedisService {
+  private client: Redis;
+
+  constructor() {
+    this.client = new Redis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: Number(process.env.REDIS_PORT) || 6379,
+        password: process.env.REDIS_PASSWORD,
+    });
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.client.get(key);
+  }
+
+  async set(key: string, value: string, expireSeconds?: number): Promise<void> {
+    if (expireSeconds) {
+      await this.client.set(key, value, 'EX', expireSeconds);
+    } else {
+      await this.client.set(key, value);
+    }
+  }
+
+  async del(key: string): Promise<void> {
+    await this.client.del(key);
+  }
+}
+
+export const redisService = new RedisService();
